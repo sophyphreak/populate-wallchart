72d9f98b924aa88da490abcbb56863aade195b0b
Andrew Horn <arhorn@smcm.edu>
2022-02-26 21:23:50 -0600

    Added error handling for unexpected column header received

--- getWallchartSheetData.js
+++ getWallchartSheetData.js
@@ -1,6 +1,9 @@
 const getWallchartSheetData = (workbook) => {
   const sheet = []
-  workbook.getWorksheet("Sheet1").eachRow((row) => {
+  workbook.getWorksheet("Sheet1").eachRow((row, rowNumber) => {
+    if (rowNumber === 1) {
+      validateColumnHeaders(row.values)
+    }
     const [
       ,
       firstName,
@@ -31,5 +34,31 @@ const getWallchartSheetData = (workbook) => {
   })
   return sheet
 }
+
+const validateColumnHeaders = (columnHeaders) => {
+  columnHeaders.forEach((header, index) => {
+    const expectedHeader = expectedColumnHeaders[index]
+    if (header !== expectedHeader) {
+      throw new Error(
+        `Unexpected column header. Expected: ${expectedHeader} but instead received: ${header}`
+      )
+    }
+  })
+}
+
+const expectedColumnHeaders = [
+  ,
+  "First Name",
+  "Last Name",
+  "Floor #",
+  "Side of building",
+  "Unit #",
+  "Relations",
+  "HTU",
+  "Floor captain",
+  "Signature",
+  "Phone Number",
+  "Email",
+]
 
 export default getWallchartSheetData
